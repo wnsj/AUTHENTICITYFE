48f122039cac17c4ac79126ee11e9f8ef489c1ff
sunyl-5664 <lx5664@hotmail.com>
2020-04-29 17:59:32 +0800

    文章管理

---
+++ src/components/MP/ArticleControl/ArticleControl.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ArticleControl"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/subArticle/SubArticle.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "SubArticle"
+    }
+</script>
+
+<style scoped>
+
+</style>
