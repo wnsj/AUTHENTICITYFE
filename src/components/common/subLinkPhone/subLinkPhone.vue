cb1eb8d2dd18cef878bcabce48e2eb73814ae931
muwenliang <a56299186@163.com>
2020-05-12 08:32:18 +0800

    --

--- src/components/common/subLinkPhone/subLinkPhone.vue
+++ src/components/common/subLinkPhone/subLinkPhone.vue
@@ -11,14 +11,14 @@
                         <label class="col-md-3 control-label text-right nopad end-aline"
                                style="padding:0;line-height:34px;">姓名</label><span class="sign-left">:</span>
                         <div class="col-md-8">
-                            <label class="form-control">{{addParam.lpName}}</label>
+                            <label class="form-control" disabled="false">{{addParam.lpName}}</label>
                         </div>
                     </div>
                     <div class="col-md-6 form-group clearfix">
                         <label class="col-md-3 control-label text-right nopad end-aline"
                                style="padding:0;line-height:34px;">联系方式</label><span class="sign-left">:</span>
                         <div class="col-md-8">
-                            <input type="text" class="form-control" v-model="addParam.phone"/>
+                            <input type="text" class="form-control" disabled="false" v-model="addParam.phone"/>
                         </div>
                     </div>
                     <div class="col-md-12 form-group clearfix">
