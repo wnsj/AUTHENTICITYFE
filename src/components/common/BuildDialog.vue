af70a4f429e2c4d71ae3c002c966e7ab47ba1241
sunyl-5664 <lx5664@hotmail.com>
2020-09-24 13:57:03 +0800

    楼盘查询

--- src/components/common/BuildDialog.vue
+++ src/components/common/BuildDialog.vue
@@ -976,15 +976,15 @@
                     return
                 }
 
-                if (this.isBlank(this.addParam.tel)) {
-                   alert('请输入正确的联系方式')
-                   return;
-                } else {
-                    if (!((/^((1[345678]\d{9})|(\d{2,3}\d{7,8})|(\d{2,3}-\d{7,8}))$/).test(this.addParam.tel))) {
-                        alert('请输入正确的联系方式')
-                        return;
-                    }
-                }
+                // if (this.isBlank(this.addParam.tel)) {
+                //    alert('请输入正确的联系方式')
+                //    return;
+                // } else {
+                //     if (!((/^((1[345678]\d{9})|(\d{2,3}\d{7,8})|(\d{2,3}-\d{7,8}))$/).test(this.addParam.tel))) {
+                //         alert('请输入正确的联系方式')
+                //         return;
+                //     }
+                // }
 
                 if (this.isBlank(this.addParam.couId)) {
                     alert('咨询师必选')
